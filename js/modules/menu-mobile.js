ca1510da01cf99093116cede6dd7df2de446bbbe
arturrochel <arturrochel@gmail.com>
2024-07-02 09:24:31 -0300

    programming menu-mobile

--- js/modules/menu-mobile.js
+++ js/modules/menu-mobile.js
@@ -1,46 +1,14 @@
 export default function initMenuMobile() {
-  const abrirMenu = document.querySelector(".abrirMenu");
+  const hamburguer = document.querySelector(".hamburguer");
   const menuMobile = document.querySelector(".menu-mobile");
-  const eventos = ["touchstart", "click"];
 
-  if (abrirMenu && menuMobile) {
-    function handleClick() {
-      menuMobile.classList.add("ativo");
-      outsideClick(menuMobile, () => {
-        menuMobile.classList.remove("ativo");
-      });
-    }
-
-    function outsideClick(element, callback) {
-      const html = document.documentElement;
-      const outside = "data-oustide";
-
-      if (!html.hasAttribute(outside)) {
-        setTimeout(() => {
-          html.setAttribute(outside, "");
-          eventos.forEach((evento) => {
-            html.addEventListener(evento, handleOutsideClick);
-          });
-        });
-      }
-
-      function handleOutsideClick(event) {
-        if (!element.contains(event.target)) {
-          callback();
-          html.removeAttribute(outside);
-          eventos.forEach((evento) => {
-            html.removeEventListener(evento, handleOutsideClick);
-          });
-        }
-      }
-    }
-
-    function removeClass() {
-      menuMobile.classList.remove("ativo");
-    }
-
-    eventos.forEach((evento) => {
-      abrirMenu.addEventListener(evento, handleClick);
-    });
+  function toggleMenu(event) {
+    event.preventDefault();
+    hamburguer.classList.toggle("active");
+    menuMobile.classList.toggle("active");
   }
+
+  ["touchstart", "click"].forEach((userEvent) => {
+    hamburguer.addEventListener(userEvent, toggleMenu);
+  });
 }
--- js/script.js
+++ js/script.js
@@ -1,17 +1,17 @@
 import initModal from "./modules/modal.js";
 import initAccordion from "./modules/accordion.js";
-import initMenuMobile from "./modules/menu-mobile.js";
 import initFuncionamento from "./modules/funcionamento.js";
 import initAnimaNumero from "./modules/anima-numero.js";
 import initAnimaScroll from "./modules/animaScroll.js";
 import initEfeitWidth from "./modules/efeito-width.js";
 import initPlanos from "./modules/planos.js";
+import initMenuMobile from "./modules/menu-mobile.js";
 
 initModal();
 initAccordion();
-initMenuMobile();
 initFuncionamento();
 initAnimaNumero();
 initAnimaScroll();
 initEfeitWidth();
 initPlanos();
+initMenuMobile();
